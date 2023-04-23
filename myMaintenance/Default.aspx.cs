0687a70e0048c0bddb6add3df93e2b80e03a1c46
sysadmin <tfs\sysadmin>
2023-04-23 19:07:09 +0000

    NAM common library added
    git-tfs-id: [http://tfs:8080/tfs/DefaultCollection]$/Enterprise Applications/NAM/mymaintenance/Main;C23

--- myMaintenance/Default.aspx.cs
+++ myMaintenance/Default.aspx.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using NAM.Common;
+using System;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace myMaintenance
 {
@@ -11,7 +8,12 @@ namespace myMaintenance
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            var objEmployee = new Employee { 
+                Id =1,
+                Name = "Demo"
+            };
+            Response.Write("Id: " + objEmployee.Id);
+            Response.Write("Name: " + objEmployee.Name);
         }
     }
 }
